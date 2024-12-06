7a28a83b6fbaec4b1a259d1ec8fa9c4a4271215e
Vafeda <78281314+Vafeda@users.noreply.github.com>
2024-12-06 05:23:13 +0300

    Add files via upload

--- Unit_test.cpp
+++ Unit_test.cpp
@@ -1,32 +1,31 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
-#include <cstdlib> 
 #include <fstream>
-using namespace std;
 
-// Функция сравнивающая output.txt и expected.txt
-bool compareFiles(const string& file1, const string& file2) {
+bool compareFiles(const std::string& file1, const std::string& file2) {
     
-    ifstream f1(file1, ios::binary);
-    ifstream f2(file2, ios::binary);
+    std::ifstream f1(file1, std::ios::binary);
+    std::ifstream f2(file2, std::ios::binary);
 
     if (!f1.is_open() || !f2.is_open()) {
-        cerr << "Ошибка: Файл не открылся." << endl;
+        std::cerr << "Файл '" + file1 + "' или '" + file2 + "' не открылся.\n";
         return false;
     }
 
-    f1.seekg(0, ios::end);
-    f2.seekg(0, ios::end);
-    streamsize size1 = f1.tellg();
-    streamsize size2 = f2.tellg();
+    f1.seekg(0, std::ios::end);
+    std::streamsize size1 = f1.tellg();
+    f2.seekg(0, std::ios::end);
+    std::streamsize size2 = f2.tellg();
 
+    // Cравнение длинны обоих файлов.
     if (size1 != size2) {
         return false;
     }
 
     f1.seekg(0);
     f2.seekg(0);
-
+    
+    // Cравнение двух файлов посимвольно.
     char c1, c2;
     while (f1.get(c1) && f2.get(c2)) {
         if (c1 != c2) {
@@ -42,29 +41,29 @@ int main() {
     
     setlocale(LC_ALL, "ru");
 
-    const string input_dir = "unit_test/input/";
-    const string output_dir = "unit_test/output/";
+    const std::string input_dir = "tests/data/input/";
+    const std::string output_dir = "tests/data/output/";
 
     for (int i = 1; i <= 7; ++i) {
  
-        string input_file = input_dir + to_string(i) + "_input.txt";
-        string expected_output_file = output_dir + to_string(i) + "_expected.txt";
-        string output_file = output_dir + to_string(i) + "_output.txt";
+        std::string input_file = input_dir + "input_" + std::to_string(i) + ".txt";
+        std::string expected_file = input_dir + "expected_" + std::to_string(i) + ".txt";
+        std::string output_file = output_dir + "output_" + std::to_string(i) + ".txt";
 
-        string command = "Sort_Tape.exe " + input_file + " " + output_file;
+        std::string command = "Sort_tape.exe " + input_file + " " + output_file;
 
         int result = system(command.c_str());
 
         if (result == -1) {
-            cerr << "Ошибка: Команда не выполнена." << endl;
+            std::cerr << "Команда '" + command + "' не выполнена.\n";
             return 1;
         }
 
-        if (compareFiles(expected_output_file, output_file)) {
-            cout << "Тест пройден для input " << i << ": сортировка выполнена корректно." << endl;
+        if (compareFiles(expected_file, output_file)) {
+            std::cout << "Тест " << i << ": сортировка выполнена корректно.\n";
         }
         else {
-            cout << "Тест не пройден для input " << i << ": сортировка выполнена некорректно." << endl;
+            std::cout << "Тест " << i << ": сортировка выполнена некорректно.\n";
         }
     }
 
